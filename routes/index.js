4f9e22a9c7de105892166c07ac50a1848bf320f7
Md Mehedi Hasan <mdmehedi060@gmail.com>
2024-06-15 18:07:53 +0600

    database work

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,33 @@ const express = require('express');
 
 const router = express.Router();
 
-const userSignUpController = require('../controller/userSignUp');
-const userSignInController = require('../controller/userSignin');
-const userDetailsController = require('../controller/userDetails');
+const userSignUpController = require('../controller/user/userSignUp');
+const userSignInController = require('../controller/user/userSignin');
+const userDetailsController = require('../controller/user/userDetails');
 const authToken = require('../middleware/authToken');
-const userLogout = require('../controller/userLogout');
+const userLogout = require('../controller/user/userLogout');
+const allUsers = require('../controller/user/allUsers');
+const updateUser = require('../controller/user/updateUser');
+const UploadProductController = require('../controller/product/uploadproduct');
+const getProductController = require('../controller/product/getProduct');
+const updateProductController = require('../controller/product/updateProduct');
+const getCategoryProduct = require('../controller/product/getCategoryProductOne');
+const getCategoryWiseProduct = require('../controller/product/getCategoryWiseProduct');
 
+// user section
 router.post('/signup', userSignUpController);
 router.post('/signin', userSignInController);
 router.get('/user-details', authToken, userDetailsController);
 router.get('/userLogout', userLogout);
 
+//admin panel
+router.get('/all-user', authToken, allUsers);
+router.post('/update-user', authToken, updateUser);
+
+//product
+router.post('/upload-product', authToken, UploadProductController);
+router.get('/get-product', getProductController);
+router.post('/update-product', authToken, updateProductController);
+router.get('/get-categoryProduct', getCategoryProduct);
+router.post('/category-product', getCategoryWiseProduct);
 module.exports = router;
