dc4fc458a1d1e44d7b79571729f1c5327cfb0f04
Md Mehedi Hasan <mdmehedi060@gmail.com>
2024-06-29 08:58:10 +0600

    added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const router = require('./routes');
 const app = express();
 app.use(
   cors({
-    origin: ' http://localhost:5173',
+    origin: ' https://ecommerce-client-rho-nine.vercel.app/',
     credentials: true,
   })
 );
